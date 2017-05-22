baefc7793c345c1fa1bdb33c06eeee9ab1462ba7
REALGAMES\kkolyaskin <kirillk@gamehouse.com>
2017-05-22 15:15:07 +0200

    1) revetred to use prod domain 2) var moved to begining of a scipt

--- vpc-games/dns-test.sh
+++ vpc-games/dns-test.sh
@@ -1,11 +1,13 @@
 #!/bin/bash -x
 
+Zone="gamehouseos.com."
+ZoneId="Z2OLJMS5MY8APL"
 ChangeSet="change-resource-record-sets.json"
 RecordValue=$(cat /tmp/outputs.txt | grep DNSName | gawk '{ print $3 }')
 RecordName=$(cat /tmp/outputs.txt | grep DNSName | gawk '{ print $2 }' | sed 's/DNSName//g' | tr '[:upper:]' '[:lower:]')
 
-GetRecordValue=$(aws route53 list-resource-record-sets --hosted-zone-id=Z34FSVFASXMJN9 \
---query "ResourceRecordSets[?Name == '$RecordName.test.gamehouseos.com.']" \
+GetRecordValue=$(aws route53 list-resource-record-sets --hosted-zone-id=$ZoneId \
+--query "ResourceRecordSets[?Name == '$RecordName.$Zone']" \
 | grep Value | sed 's/\"\| //g' | gawk -F: '{ print $2 }')
 
 
@@ -14,6 +16,6 @@ if [[ "$GetRecordValue" == "$RecordValue" ]]; then echo "Record is already there
         		cp $WORKSPACE/$GITFOLDER/$ChangeSet /tmp/$ChangeSet
                 sed -i "s/ChangeName/$RecordName/g" /tmp/$ChangeSet && \
                 sed -i "s/ChangeValue/$RecordValue/g" /tmp/$ChangeSet && \
-                aws route53 change-resource-record-sets --hosted-zone-id=Z34FSVFASXMJN9 \
+                aws route53 change-resource-record-sets --hosted-zone-id=$ZoneId \
                 --change-batch file:////tmp/$ChangeSet && exit $?
 fi
