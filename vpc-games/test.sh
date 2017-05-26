ef51fdcb9f6013280406de7ee4a7963dea29c342
REALGAMES\kkolyaskin <kirillk@gamehouse.com>
2017-05-26 10:13:35 +0200

    Tests modified to avoid error when mixed up attachment of IG and VPNGW

--- vpc-games/test.sh
+++ vpc-games/test.sh
@@ -23,7 +23,7 @@ fi
 
 
 #3. Checking if IG has been attached to VPC. We are not cheking if IG has been created, anyway it would violent template.
-igvpcatt=$(grep -A 2 "\"AWS::EC2::VPCGatewayAttachment\"" /tmp/parsed | gawk -F\" '{print $2}')
+igvpcatt=$(grep -A 2 "\"AWS::EC2::VPCGatewayAttachment\"" /tmp/parsed | grep -v VPN | gawk -F\" '{print $2}')
 igvpcatt=$(echo $igvpcatt | sed 's/ //g')
 
 if [[ "$igvpcatt" != "AWS::EC2::VPCGatewayAttachment!RefInternetGateway!RefVPC" ]] ; then
