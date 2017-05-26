10928d344445c76f650fe48482851dc1408eae1b
REALGAMES\kkolyaskin <kirillk@gamehouse.com>
2017-05-26 16:20:23 +0200

    Issues InfraVPC and Roger fixed

--- build-stuff/build-shells/roger.sh
+++ build-stuff/build-shells/roger.sh
@@ -4,7 +4,7 @@ function BuildTrigger {
 	case $GitFolder in
 		infra-vpc )
   			echo "Roger says - InfraVpc" && \
-  			curl -X POST https://127.0.0.1:8080/job/InfraVpc_Deploy_Test/build?token=e0zud9iDkY1cseglsAyg;; 
+  			curl -X POST http://127.0.0.1:8080/job/InfraVpc_Deploy_Test/build?token=e0zud9iDkY1cseglsAyg;; 
   		vpc-games )
 			echo "Roger says - GamesVpc" && \
 			curl -X POST http://127.0.0.1:8080/job/GamesVpc_Tests/build?token=NnT0JdpQUC2URw0tsxwg;; 
