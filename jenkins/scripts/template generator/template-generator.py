5f9e091ce15a1a81b30b611d8974eb514bff816d
REALGAMES\kkolyaskin <kirillk@gamehouse.com>
2017-06-27 10:28:50 +0200

    clother to PEP8

--- jenkins/scripts/template generator/template-generator.py
+++ jenkins/scripts/template generator/template-generator.py
@@ -1,9 +1,9 @@
-import boto3
-import argparse
-import json
+# This script generates CF template for Jenkins stack of CBS project
 
-from troposphere import Template, Ref, FindInMap, Base64, Parameter, Join, GetAZs
-from troposphere.s3 import Bucket, PublicRead
+import argparse
+
+from troposphere import Template, Ref, FindInMap, Base64, Parameter
+from troposphere.s3 import Bucket
 from troposphere.ec2 import SecurityGroup, SecurityGroupIngress
 import troposphere.ec2 as ec2
 import troposphere.iam as iam
@@ -12,18 +12,16 @@ import troposphere.elasticloadbalancing as elb
 from awacs.aws import Allow, Statement, Principal, Policy
 from awacs.sts import AssumeRole
 
-
-
 t = Template()
 
 
-#Classes definition
+# Classes definition
 class Input:
     def __init__(self):
         # Parse CLI arguments
         parser = argparse.ArgumentParser(description='Script to generate a CF template for Studios Jenkins')
         parser.add_argument('--REGION', '-R', action='store', required=True)
-        parser.add_argument('--STUDIONAME', '-S', action='store', required=True)
+        parser.add_argument('--StudioName', '-S', action='store', required=True)
         parser.add_argument('--WINDOWS', '-W', action='store', required=True)
         parser.add_argument('--LINUX', '-L', action='store', required=True)
         parser.add_argument('--MAC', '-M', action='store', required=False)
@@ -32,9 +30,9 @@ class Input:
         parser.add_argument('--WindowsAMI', '-w', action='store', required=True)
         args = parser.parse_args()
 
-        #Assign aruments to a local variables
+        # Assign aruments to a local variables
         self.Region = args.REGION
-        self.StudioName = args.STUDIONAME
+        self.StudioName = args.StudioName
         self.Windows = args.WINDOWS
         self.Linux = args.LINUX
         self.Mac = args.MAC
@@ -42,11 +40,12 @@ class Input:
         self.MasterAMI = args.MasterAMI
         self.WindowsAMI = args.WindowsAMI
 
+
 class Parametrs:
     def __init__(self):
         # Creating parametrs
-        self.VPCid = t.add_parameter(Parameter(
-            "VPCid",
+        self.VpcId = t.add_parameter(Parameter(
+            "VpcId",
             Type="String",
         ))
         self.PublicSubnet1Id = t.add_parameter(Parameter(
@@ -73,116 +72,117 @@ class Parametrs:
             "WhiteIp1",
             Type="String",
         ))
-        self.KEYName = t.add_parameter(Parameter(
+        self.KeyName = t.add_parameter(Parameter(
             "KeyName",
             Type="AWS::EC2::KeyPair::KeyName",
         ))
 
+
 class StaticResources:
-    def __init__(self, StudioName, PublicSubnet1Id, VPCid, InfraVpcCIDR, GamesVpcCIDR, Ec2TypeMaster, KEYName, WhiteIp1):
+    def __init__(self, StudioName, PublicSubnet1Id, VpcId, InfraVpcCIDR, GamesVpcCIDR, Ec2TypeMaster,
+                 KeyName, WhiteIp1):
         s = self
 
         # Creating SGs
         # ELB SG
         s.SGElbName = StudioName + "SgElb"
         s.SGElb = t.add_resource(
-        SecurityGroup(
-            s.SGElbName,
-            GroupDescription='Enable access to the Jenkins LB',
-            SecurityGroupIngress=[
-                ec2.SecurityGroupRule(
-                    IpProtocol="tcp",
-                    FromPort="443",
-                    ToPort="443",
-                    CidrIp=Ref(WhiteIp1),
-                ),
-            ],
-            VpcId=Ref(VPCid),
-        ))
+            SecurityGroup(
+                s.SGElbName,
+                GroupDescription='Enable access to the Jenkins LB',
+                SecurityGroupIngress=[
+                    ec2.SecurityGroupRule(
+                        IpProtocol="tcp",
+                        FromPort="443",
+                        ToPort="443",
+                        CidrIp=Ref(WhiteIp1),
+                    ),
+                ],
+                VpcId=Ref(VpcId),
+            ))
 
         # Slave SG
         s.SGWindowsName = StudioName + "SgEc2JenkinsWindows"
         s.SGWindows = t.add_resource(
-        SecurityGroup(
-            s.SGWindowsName,
-            GroupDescription='Jenkins Windows Slave EC2 SG',
-            SecurityGroupIngress=[
-                ec2.SecurityGroupRule(
-                    IpProtocol="tcp",
-                    FromPort="3389",
-                    ToPort="3389",
-                    CidrIp=Ref(WhiteIp1),
-                ),
-                ec2.SecurityGroupRule(
-                    IpProtocol="icmp",
-                    FromPort="-1",
-                    ToPort="-1",
-                    CidrIp=Ref(GamesVpcCIDR),
-                ),
-            ],
-            VpcId=Ref(VPCid),
-        ))
+            SecurityGroup(
+                s.SGWindowsName,
+                GroupDescription='Jenkins Windows Slave EC2 SG',
+                SecurityGroupIngress=[
+                    ec2.SecurityGroupRule(
+                        IpProtocol="tcp",
+                        FromPort="3389",
+                        ToPort="3389",
+                        CidrIp=Ref(WhiteIp1),
+                    ),
+                    ec2.SecurityGroupRule(
+                        IpProtocol="icmp",
+                        FromPort="-1",
+                        ToPort="-1",
+                        CidrIp=Ref(GamesVpcCIDR),
+                    ),
+                ],
+                VpcId=Ref(VpcId),
+            ))
 
         # Master SG
         s.SGMasterName = StudioName + "SgEc2JenkinsMaster"
         s.SGMaster = t.add_resource(
-        SecurityGroup(
-            s.SGMasterName,
-            GroupDescription='Jenkins Master EC2 SG',
-            SecurityGroupIngress=[
-                ec2.SecurityGroupRule(
-                    IpProtocol="tcp",
-                    FromPort="80",
-                    ToPort="80",
-                    SourceSecurityGroupId=Ref(s.SGElb),
-                ),
-                ec2.SecurityGroupRule(
-                    IpProtocol="tcp",
-                    FromPort="80",
-                    ToPort="80",
-                    SourceSecurityGroupId=Ref(s.SGWindows),
-                ),
-                ec2.SecurityGroupRule(
-                    IpProtocol="tcp",
-                    FromPort="22",
-                    ToPort="22",
-                    SourceSecurityGroupId=Ref(InfraVpcCIDR),
-                ),
-                ec2.SecurityGroupRule(
-                    IpProtocol="icmp",
-                    FromPort="-1",
-                    ToPort="-1",
-                    CidrIp=Ref(GamesVpcCIDR),
-                ),
-            ],
-            VpcId=Ref(VPCid),
-        ))
+            SecurityGroup(
+                s.SGMasterName,
+                GroupDescription='Jenkins Master EC2 SG',
+                SecurityGroupIngress=[
+                    ec2.SecurityGroupRule(
+                        IpProtocol="tcp",
+                        FromPort="80",
+                        ToPort="80",
+                        SourceSecurityGroupId=Ref(s.SGElb),
+                    ),
+                    ec2.SecurityGroupRule(
+                        IpProtocol="tcp",
+                        FromPort="80",
+                        ToPort="80",
+                        SourceSecurityGroupId=Ref(s.SGWindows),
+                    ),
+                    ec2.SecurityGroupRule(
+                        IpProtocol="tcp",
+                        FromPort="22",
+                        ToPort="22",
+                        SourceSecurityGroupId=Ref(InfraVpcCIDR),
+                    ),
+                    ec2.SecurityGroupRule(
+                        IpProtocol="icmp",
+                        FromPort="-1",
+                        ToPort="-1",
+                        CidrIp=Ref(GamesVpcCIDR),
+                    ),
+                ],
+                VpcId=Ref(VpcId),
+            ))
 
         # Ingress rules
         s.SGIName = StudioName + "IngressMasterSlaves"
         t.add_resource(
             SecurityGroupIngress(
-            s.SGIName,
-            GroupId=Ref(s.SGMaster),
-            SourceSecurityGroupId=Ref(s.SGWindows),
-            IpProtocol="tcp",
-            FromPort="0",
-            ToPort="65535",
+                s.SGIName,
+                GroupId=Ref(s.SGMaster),
+                SourceSecurityGroupId=Ref(s.SGWindows),
+                IpProtocol="tcp",
+                FromPort="0",
+                ToPort="65535",
             )
         )
 
         s.SGIName = StudioName + "IngressSlavesMaster"
         t.add_resource(
             SecurityGroupIngress(
-            s.SGIName,
-            GroupId=Ref(s.SGWindows),
-            SourceSecurityGroupId=Ref(s.SGMaster),
-            IpProtocol="tcp",
-            FromPort="0",
-            ToPort="65535",
+                s.SGIName,
+                GroupId=Ref(s.SGWindows),
+                SourceSecurityGroupId=Ref(s.SGMaster),
+                IpProtocol="tcp",
+                FromPort="0",
+                ToPort="65535",
             )
         )
-
 
         # Creating Jenkins Role
         s.JenkinsRoleName = StudioName + "JenkinsRole"
@@ -190,13 +190,13 @@ class StaticResources:
             s.JenkinsRoleName,
             Path="/",
             AssumeRolePolicyDocument=Policy(
-            Statement=[
-                Statement(
-                    Effect=Allow,
-                    Action=[AssumeRole],
-                    Principal=Principal("Service", ["ec2.amazonaws.com"])
-                )
-            ]),
+                Statement=[
+                    Statement(
+                        Effect=Allow,
+                        Action=[AssumeRole],
+                        Principal=Principal("Service", ["ec2.amazonaws.com"])
+                    )
+                ]),
             Policies=[
                 iam.Policy(
                     PolicyName="logs",
@@ -220,7 +220,7 @@ class StaticResources:
                                 "route53:ListResourceRecordSets",
                                 "route53:ChangeResourceRecordSets",
                                 "route53:GetChange",
-                                ],
+                            ],
                             "Resource": "*"
                         }],
                     }
@@ -235,7 +235,6 @@ class StaticResources:
             Path="/",
             Roles=[Ref(s.JenkinsRole)],
         ))
-        
 
         # Creating Master Jenkins Instance
         s.JenkinsMasterName = StudioName + "JenkinsMaster"
@@ -243,7 +242,7 @@ class StaticResources:
             s.JenkinsMasterName,
             ImageId=FindInMap("JenkinsMaster", Ref("AWS::Region"), "AMI"),
             InstanceType=Ref(Ec2TypeMaster),
-            KeyName=Ref(KEYName),
+            KeyName=Ref(KeyName),
             SecurityGroups=[Ref(s.SGMaster)],
             SubnetId=Ref(PublicSubnet1Id),
             IamInstanceProfile=Ref(s.JenkinsProfile)
@@ -277,14 +276,12 @@ class StaticResources:
         s.s3Name = StudioName + 'S3'
         s.s3 = t.add_resource(Bucket(
             s.s3Name,
-            AccessControl = "Private",
+            AccessControl="Private",
         ))
 
 
-
-
 class DynamicResources:
-    def __init__(self, StudioName, Windows, KEYName, Ec2TypeWindows, SGWindows):
+    def __init__(self, StudioName, Windows, KeyName, Ec2TypeWindows, SGWindows):
         s = self
         # Creating Windows slaves
         W = int(Windows)
@@ -295,39 +292,39 @@ class DynamicResources:
                 s.ec2name,
                 ImageId=FindInMap("RegionMap", Ref("AWS::Region"), "AMI"),
                 InstanceType=Ref(Ec2TypeWindows),
-                KeyName=Ref(KEYName),
+                KeyName=Ref(KeyName),
                 SecurityGroups=[Ref(SGWindows)],
                 UserData=Base64("80")
             ))
             i += 1
 
 
-
-
 # Functions to call classes
-def GetInput():
+def get_input():
     return Input()
 
-def GetParametrs():
+
+def get_parametrs():
     return Parametrs()
 
-def GetStaticResources(StudioName, PublicSubnet1Id, VPCid, InfraVpcCIDR, GamesVpcCIDR, Ec2TypeMaster, KEYName, WhiteIp1):
-    return StaticResources(StudioName, PublicSubnet1Id, VPCid, InfraVpcCIDR, GamesVpcCIDR, Ec2TypeMaster, KEYName, WhiteIp1)
 
-def GetDynamicResources(StudioName, Windows, KEYName, Ec2TypeWindows, SGWindows):
-    return DynamicResources(StudioName, Windows, KEYName, Ec2TypeWindows, SGWindows)
+def get_static_resources(StudioName, PublicSubnet1Id, VpcId, InfraVpcCIDR, GamesVpcCIDR,
+                         Ec2TypeMaster, KeyName,WhiteIp1):
+    return StaticResources(StudioName, PublicSubnet1Id, VpcId, InfraVpcCIDR, GamesVpcCIDR,
+                           Ec2TypeMaster, KeyName,WhiteIp1)
 
 
-
+def get_dynamic_resources(StudioName, Windows, KeyName, Ec2TypeWindows, SGWindows):
+    return DynamicResources(StudioName, Windows, KeyName, Ec2TypeWindows, SGWindows)
 
 
 # Functions
-def CreateDescription(StudioName):
-    Description =  "Python-generated template for " + StudioName + " studio"
+def create_description(StudioName):
+    Description = "Python-generated template for " + StudioName + " studio"
     t.add_description(Description)
 
-def CreateMapping(Region, MasterAMI, WindowsAMI):
 
+def create_mapping(Region, MasterAMI, WindowsAMI):
     t.add_mapping('JenkinsMaster', {
         Region: {"AMI": MasterAMI}
     })
@@ -336,22 +333,18 @@ def CreateMapping(Region, MasterAMI, WindowsAMI):
     })
 
 
-
-
-
 def main():
+    i = get_input()
+    p = get_parametrs()
 
-    i = GetInput()
-    p = GetParametrs()
+    create_description(i.StudioName)
+    create_mapping(i.Region, i.MasterAMI, i.WindowsAMI)
 
-
-    CreateDescription(i.StudioName)
-    CreateMapping(i.Region, i.MasterAMI, i.WindowsAMI)
-
-
-    sr = GetStaticResources(i.StudioName, p.PublicSubnet1Id, p.VPCid, p.InfraVpcCIDR, p.GamesVpcCIDR, p.Ec2TypeMaster, p.KEYName, p.WhiteIp1)
-    dr = GetDynamicResources(i.StudioName, i.Windows, p.KEYName, p.Ec2TypeWindows, sr.SGWindows)
+    sr = get_static_resources(i.StudioName, p.PublicSubnet1Id, p.VpcId, p.InfraVpcCIDR, p.GamesVpcCIDR,
+                              p.Ec2TypeMaster, p.KeyName, p.WhiteIp1)
+    dr = get_dynamic_resources(i.StudioName, i.Windows, p.KeyName, p.Ec2TypeWindows, sr.SGWindows)
 
     print(t.to_json())
+
 
 main()
