bad5e2be64dca98bae133d48f510d62f8d3dafbc
REALGAMES\kkolyaskin <kirillk@gamehouse.com>
2017-06-30 10:06:25 +0200

    test 2

--- vpc-games/scripts/template-generator.py
+++ vpc-games/scripts/template-generator.py
@@ -22,7 +22,7 @@ t = Template()
 class Input:
     def __init__(self):
         # Parse CLI arguments
-        parser = argparse.ArgumentParser(description='Script to generate a CF template for Studios Jenkins')
+        parser = argparse.ArgumentParser(description='Script to generate a CF template for Studios Jenkins without outputs')
         parser.add_argument('--REGION', '-R', action='store', required=True)
         parser.add_argument('--STUDIONAME', '-S', action='store', required=True)
         parser.add_argument('--WINDOWS', '-W', action='store', required=True)
