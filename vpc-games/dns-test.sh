2eba5014f2c1610ac8d04cc8403279ed04dc3d7c
REALGAMES\kkolyaskin <kirillk@gamehouse.com>
2017-07-06 10:20:00 +0200

    new versions of dns update script

--- vpc-games/dns-test.sh
+++ vpc-games/dns-test.sh
@@ -1,23 +1,28 @@
-#!/bin/bash -x
+#!/bin/bash
 
-# Version 1.2
+# Version 2.0
 
 Zone="gamehouseos.com."
 ZoneId="Z2OLJMS5MY8APL"
 ChangeSet="change-resource-record-sets.json"
-RecordValue=$(cat /tmp/outputs-games.txt | grep DNSName | gawk '{ print $3 }')
-RecordName=$(cat /tmp/outputs-games.txt | grep DNSName | gawk '{ print $2 }' | sed 's/DNSName//g' | tr '[:upper:]' '[:lower:]')
 
-GetRecordValue=$(aws route53 list-resource-record-sets --hosted-zone-id=$ZoneId \
+
+cat /tmp/outputs-games.txt | grep DNSName > /tmp/outputs-games.parced
+
+while read line; do
+        RecordValue=$(echo $line | grep DNSName | gawk '{ print $3 }')
+        RecordName=$(echo $line | grep DNSName | gawk '{ print $2 }' | sed 's/DNSName//g' | tr '[:upper:]' '[:lower:]')
+        GetRecordValue=$(aws route53 list-resource-record-sets --hosted-zone-id=$ZoneId \
 --query "ResourceRecordSets[?Name == '$RecordName.$Zone']" \
 | grep Value | sed 's/\"\| //g' | gawk -F: '{ print $2 }')
 
-
-if [[ "$GetRecordValue" == "$RecordValue" ]]; then echo "Record is already there. Nothing to do" && exit 0
+        if [[ "$GetRecordValue" == "$RecordValue" ]]; then echo "Record is already there. Nothing to do" && exit 0
         else
-        		cp $WORKSPACE/$GITFOLDER/$ChangeSet /tmp/$ChangeSet
+                cp $WORKSPACE/$GITFOLDER/$ChangeSet /tmp/$ChangeSet
                 sed -i "s/ChangeName/$RecordName/g" /tmp/$ChangeSet && \
                 sed -i "s/ChangeValue/$RecordValue/g" /tmp/$ChangeSet && \
                 aws route53 change-resource-record-sets --hosted-zone-id=$ZoneId \
-                --change-batch file:////tmp/$ChangeSet && exit $?
-fi
+                --change-batch file:////tmp/$ChangeSet
+        fi
+
+done < /tmp/outputs-games.parced
